347a62f8fb02f2665c99647d787bd6d61b17674b
LunaNona <atoosa083@gmail.com>
2023-08-07 09:17:38 +0330

    fourth commit

--- pages/AboutUs.vue
+++ pages/AboutUs.vue
@@ -2,15 +2,15 @@
 
     <div class="container">
         <div>
-            <v-card class="contactus-text-card black mt-3 " :opacity="0.8">
+            <v-card class="aboutus-text-card black mt-3 " :opacity="0.8">
                 <h1 class="flex justify-center align-center white--text">تماس با ما</h1>
-                <div class="contactus-text"><p>متن</p></div>
+                <div class="aboutus-text"><p>متن</p></div>
                 <br>
             </v-card>
         </div>
-        <div class="contact-content-div">
+        <div class="about-content-div">
             <!-- <font-awesome-icon :icon="['fas', 'forward']" rotation=180 /> -->
-            <h1 class="contact-content-h1">در یک نگاه</h1>
+            <h1 class="about-content-h1">در یک نگاه</h1>
         </div>
     </div>
 </template>
@@ -29,7 +29,7 @@
         margin: 0px;
         padding: 0px;
     }
-    .contactus-text{
+    .aboutus-text{
         opacity: 0.7;
         background-color: aliceblue;
         visibility: 0.7;
@@ -39,22 +39,22 @@
         direction: rtl;
         align-items: center;
     }
-    .contactus-text-card{
+    .aboutus-text-card{
         margin-right:200px ;
         margin-left: 200px;
 
     }
-    .contact-content{
+    .about-content{
         direction: rtl;
         margin-right: 70px;
         margin-left: 70px;
     }
-    .contact-content-h1{
+    .about-content-h1{
         margin-top: 30px;
         direction: rtl;
         margin-right: 110px;
     }
-    .contact-content-div{
+    .about-content-div{
         background-image: url(https://cdn1.vectorstock.com/i/1000x1000/66/85/polygonal-3d-yellow-background-seamless-pattern-vector-20326685.jpg) ;
         background-attachment: fixed;
         background-size: cover;
